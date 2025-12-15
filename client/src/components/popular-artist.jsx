7356136ab07ee3fa09ce782ff53ed56702e63b4f
dtHvinh <dangthehienvinh662@gmail.com>
2025-12-15 21:30:39 +0700

    refactor: remove hardcoded artist data and ensure unique key for ArtistCard component

--- client/src/components/popular-artist.jsx
+++ client/src/components/popular-artist.jsx
@@ -1,45 +1,6 @@
 import { useRoutingContext } from "../hooks/useRoutingContext";
 import ArtistCard from "./artist-card";
 
-const artists = [
-  {
-    id: 1,
-    name: "Luna Nova",
-    genre: "Electronic",
-    followers: "2.5M",
-  },
-  {
-    id: 2,
-    name: "The Voltage",
-    genre: "Rock",
-    followers: "1.8M",
-  },
-  {
-    id: 3,
-    name: "Jay Storm",
-    genre: "Hip Hop",
-    followers: "4.2M",
-  },
-  {
-    id: 4,
-    name: "Aria Belle",
-    genre: "Pop",
-    followers: "3.1M",
-  },
-  {
-    id: 5,
-    name: "Sunset Collective",
-    genre: "Indie",
-    followers: "890K",
-  },
-  {
-    id: 6,
-    name: "DJ Phoenix",
-    genre: "House",
-    followers: "1.2M",
-  },
-];
-
 export default function ArtistsSection() {
   const { navigateTo } = useRoutingContext();
 
@@ -60,7 +21,7 @@ export default function ArtistsSection() {
       </div>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
         {artists.map((artist) => (
-          <ArtistCard id={artist.id} artist={artist} />
+          <ArtistCard key={artist.id} id={artist.id} artist={artist} />
         ))}
       </div>
     </section>
